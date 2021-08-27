7bd76515f50b1b387bf1991ec6eefbe3ba82656a
PZR <46976238+PZR2000@users.noreply.github.com>
2021-08-27 11:05:33 -0400

    Building homepage

--- app.py
+++ app.py
@@ -9,3 +9,7 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 @app.route("/")
 def layout():
     return render_template("index.html")
+
+@app.route("/about")
+def about():
+    render_template("about.html")
